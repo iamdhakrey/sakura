6a2473f6481f53628862128d741a915fc265b156
Hrithik Dhakrey <35266081+iamdhakrey@users.noreply.github.com>
2021-05-11 20:01:41 +0530

    add avatar url

--- main/views.py
+++ main/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request,'base.html',{'user':request.user})
+    if request.user.is_authenticated:
+        avatar_url = "https://cdn.discordapp.com/avatars/"+str(request.user.id)+"/"+str(request.user.avatar)+".webp"
+    else:
+        avatar_url = None
+    return render(request,'base.html',{'user':request.user,'avatar_url':avatar_url})
