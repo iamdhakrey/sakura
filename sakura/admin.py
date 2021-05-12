20fc68f6a88a6bbee3ea645dc4ec3471ef987a6b
Hrithik Dhakrey <35266081+iamdhakrey@users.noreply.github.com>
2021-05-12 21:16:59 +0530

    add models in Admin site

--- sakura/admin.py
+++ sakura/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import WelcomeData,Server,User
+
+@admin.register(WelcomeData)
+class WelcomeDataAdmin(admin.ModelAdmin):
+    list_display = ('server_id','server_name',"welcome_enable",'update_by','last_update')
+
+@admin.register(Server)
+class ServerAdmin(admin.ModelAdmin):
+    list_display = ('server_id','server_name','is_active')
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('member_id','discord_tag')
+
