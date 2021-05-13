0dd50fe2920c28a25321f114d18162f130b11ef0
Hrithik Dhakrey <35266081+iamdhakrey@users.noreply.github.com>
2021-05-13 20:25:25 +0530

    [Backend] - add command errors

--- sakura/BotEvents/events.py
+++ sakura/BotEvents/events.py
@@ -1,10 +1,11 @@
 import discord
 from discord.ext.commands import Bot
+from discord.ext.commands import Context
 from discord.ext import commands
 from sakura.utils import prBlue, prBold, prCyan, prGreen, prPurple, prRed, prYellow
 from sakura.BotMics.bot_db import DbConnection
 
-from sakura.config import COGS_FOLDER,DEFINED_COGS
+from sakura.config import COGS_FOLDER,DEFINED_COGS,SAKURA_DEBUG
 
 def event_setup(bot:Bot):
     @bot.event
@@ -36,3 +37,19 @@ def event_setup(bot:Bot):
     async def on_guild_remove(guild):
         prPurple(f'[Bot] - Guild Remove -> {guild.name}')
         await DbConnection.left_server(guild)
+
+    @bot.event
+    async def on_command(ctx:Context):
+        cmd = str(ctx.command).replace(ctx.prefix,'').capitalize()
+        if SAKURA_DEBUG:
+            prBold(f"[CMD] - {cmd} used by {ctx.author.name}#{ctx.author.discriminator}")
+        
+    @bot.event
+    async def on_command_error(ctx:Context,error):
+        if SAKURA_DEBUG:
+            if isinstance(error,commands.CommandError):
+                if isinstance(error,commands.CommandNotFound):
+                    cmd = str(ctx.message.content).replace(ctx.prefix,'').split()[0]
+                    prYellow(f"[CMD_Not_Found] - {cmd} execute by {ctx.author.name}#{ctx.author.discriminator}")
+        else:
+            prBlue(error)
