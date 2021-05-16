2a0357102251596ee8d9034cd12e55db2fb9c7b7
Hrithik Dhakrey <35266081+iamdhakrey@users.noreply.github.com>
2021-05-16 19:56:07 +0530

    [Frontend] - remove avatar url

--- main/views.py
+++ main/views.py
@@ -6,8 +6,5 @@ from django.conf import settings
 discord_invite = settings.DISCORD_INVITE_LINK
 
 def home(request):
-    if request.user.is_authenticated:
-        avatar_url = "https://cdn.discordapp.com/avatars/"+str(request.user.id)+"/"+str(request.user.avatar)+".webp"
-    else:
-        avatar_url = None
+    avatar_url = None
     return render(request,'base.html',{'user':request.user,'avatar_url':avatar_url,"invite_url":discord_invite})
