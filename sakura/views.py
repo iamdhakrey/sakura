6169549e470a380ae4114b2326fdee614b53520a
Hrithik Dhakrey <35266081+iamdhakrey@users.noreply.github.com>
2021-05-16 19:56:49 +0530

    [Backend] - add dashboard Views

--- sakura/views.py
+++ sakura/views.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.decorators import login_required
+from sakura.BotMics.api import Discord_API
+from django.http import request as Request
 from django.shortcuts import render
 
 # Create your views here.
+@login_required
+def dashboard(request:Request):
+    content = {}
+    print(request.user.access_token)
+    guild_list = Discord_API().get_guild_list(request.user.access_token)
+    print(guild_list)
+    return render(request,'channels.html',{'username':request.user,'guild_list':guild_list})
