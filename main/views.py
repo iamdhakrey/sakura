c553b63eec896c5f22f0b506772b059b83455744
Hrithik Dhakrey <35266081+iamdhakrey@users.noreply.github.com>
2021-05-31 20:05:59 +0530

    [Frontend] - check authentication

--- main/views.py
+++ main/views.py
@@ -1,15 +1,22 @@
 from logging import setLoggerClass
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 from django.conf import settings
 
 discord_invite = settings.DISCORD_INVITE_LINK
 
 def home(request):
+    if request.user.is_authenticated:
+        print(discord_invite)
+        avatar_url = None
+        return redirect('dashboard')
     avatar_url = None
     return render(request,'base.html',{'user':request.user,'avatar_url':avatar_url,"invite_url":discord_invite})
 
 def new_home(request):
-    print(discord_invite)
-    avatar_url = None
-    return render(request,'new_base.html',{'user':request.user,'avatar_url':avatar_url,"invite_url":discord_invite})
+    if request.user.is_authenticated:
+        print(discord_invite)
+        avatar_url = None
+        return redirect('dashboard')
+    else:
+        return render(request,'new_base.html',{'user':request.user,'avatar_url':None,"invite_url":discord_invite})
