f64bcc24769701797ccc5207c563a9f239de1ef2
Hrithik Dhakrey <35266081+iamdhakrey@users.noreply.github.com>
2021-05-31 20:16:04 +0530

    [Bot] - update norm_to_emoji

--- sakura/BotMics/botutils.py
+++ sakura/BotMics/botutils.py
@@ -2,12 +2,12 @@ import discord
 from discord.ext.commands.context import Context
 
 
-def norm_to_emoji(ctx:Context,arg:str):
+def norm_to_emoji(guild:discord.Guild,arg:str):
     new_arg = []
     arg = str(arg).split(":")
     for i in arg:
-        if discord.utils.get(ctx.guild.emojis,name=i):
-            emoji = discord.utils.get(ctx.guild.emojis,name=i)
+        if discord.utils.get(guild.emojis,name=i):
+            emoji = discord.utils.get(guild.emojis,name=i)
             new_arg.append(str(emoji))
         else:
             new_arg.append(str(i))
