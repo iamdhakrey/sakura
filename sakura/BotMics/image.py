856200575e2577a311a673340c49857861e196aa
Hrithik Dhakrey <35266081+iamdhakrey@users.noreply.github.com>
2021-05-31 20:16:24 +0530

    [Bot] - update image

--- sakura/BotMics/image.py
+++ sakura/BotMics/image.py
@@ -11,11 +11,11 @@ from io import BytesIO
 
 from django.conf import settings
 ROOT_PATH = str(settings.MEDIA_ROOT)
-def save_image(bot,ctx):
+def save_image(bot,guild,member):
     is_w_images = False
     count_list = []
     for i in range(5):
-        if os.path.isfile(os.path.join(ROOT_PATH,"images/backgrounds/"+str(ctx.guild.id)+'/'+str(ctx.guild.id)+"_"+str(i)+".jpg")):
+        if os.path.isfile(os.path.join(ROOT_PATH,"images/backgrounds/"+str(guild.id)+'/'+str(guild.id)+"_"+str(i)+".jpg")):
             is_w_images = True
             count_list.append(i)
         else:
@@ -29,9 +29,9 @@ def save_image(bot,ctx):
     W,H = 1920,872
     # messeges
     hello = "Hey Buddy,"
-    username = str(bot.get_user(ctx.author.id))
-    msg = 'You Are {}th Member of The Server'.format(ctx.guild.member_count)
-    url = requests.get(ctx.author.avatar_url)
+    username = str(bot.get_user(member.id))
+    msg = 'You Are {}th Member of The Server'.format(guild.member_count)
+    url = requests.get(member.avatar_url)
     avatar = Image.open(BytesIO(url.content))
     avatar = avatar.resize((380, 380))
     bigsize = (avatar.size[0] * 3,  avatar.size[1] * 3)
@@ -42,13 +42,13 @@ def save_image(bot,ctx):
     avatar.putalpha(mask)
     output = ImageOps.fit(avatar, mask.size, centering=(0.5, 0.5))
     output.putalpha(mask)
-    output.save(str(ROOT_PATH)+'/images/'+str(ctx.guild.id)+'/avatar.png')
-    ava = Image.open(str(ROOT_PATH)+'/images/'+str(ctx.guild.id)+'/avatar.png')
+    output.save(str(ROOT_PATH)+'/images/'+str(guild.id)+'/avatar.png')
+    ava = Image.open(str(ROOT_PATH)+'/images/'+str(guild.id)+'/avatar.png')
     ava_draw = ImageDraw.Draw(ava)
     ava_draw.arc((0, 0, 380, 380), start=0, end=360, fill=(194,83,111),width=12)
-    ava.save(str(ROOT_PATH)+'/images/'+str(ctx.guild.id)+'/avatar.png')
+    ava.save(str(ROOT_PATH)+'/images/'+str(guild.id)+'/avatar.png')
     if is_w_images:
-        welc = Image.open(ROOT_PATH+'/images/backgrounds/'+str(ctx.guild.id)+"/"+str(ctx.guild.id)+"_"+str(image_num)+'.jpg' )
+        welc = Image.open(ROOT_PATH+'/images/backgrounds/'+str(guild.id)+"/"+str(guild.id)+"_"+str(image_num)+'.jpg' )
     else:
         welc = Image.open(ROOT_PATH+'/default/welcome'+str(image_num)+'.jpg' )
     font1 = ImageFont.truetype(ROOT_PATH+'/fonts/UbuntuMono-B.ttf', 90)
@@ -61,4 +61,4 @@ def save_image(bot,ctx):
     welc_draw.text(xy=((W-user_w)/2,(H-user_h)/1.2), text=username, fill=(222,239,90), font=font1,align='center')
     welc_draw.text(xy=((W-msg_w)/2,(H-msg_h)/1), text=msg, fill=(248,206,160), font=font2,align = 'center')
     welc.paste(ava, (760, 150), ava)
-    welc.save(str(ctx.guild.id)+'_out_welcome.png', format='PNG')
+    welc.save(str(guild.id)+'_out_welcome.png', format='PNG')
