196507eab294cc5710e8c17ff59d84cd25c15d64
Hrithik Dhakrey <35266081+iamdhakrey@users.noreply.github.com>
2021-05-31 20:03:22 +0530

    [Backend] - Add user admin

--- sakuralogin/models.py
+++ sakuralogin/models.py
@@ -45,3 +45,59 @@ class DiscordUser(models.Model):
         else:
             return self.discord_tag
     
+
+from django.db import models
+from django.contrib.auth.models import (BaseUserManager, AbstractBaseUser)
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, username, password=None):
+        if not username:
+            raise ValueError('Users must have a valid address')
+
+        user = self.model(
+            username=username,
+
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password):
+        user = self.create_user(
+            username,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    username = models.CharField(
+        verbose_name='username',
+        max_length=255,
+        unique=True,
+    )
+    date_of_birth = models.DateField()
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'username'
+    # REQUIRED_FIELDS = ['date_of_birth']
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
