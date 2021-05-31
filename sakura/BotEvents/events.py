d2fc69faa8f97ab853d106f2707d302d472c470e
Hrithik Dhakrey <35266081+iamdhakrey@users.noreply.github.com>
2021-05-31 20:12:41 +0530

    [Bot] - remove guild join

--- sakura/BotEvents/events.py
+++ sakura/BotEvents/events.py
@@ -33,21 +33,6 @@ def event_setup(bot:Bot):
         prBlue(f"[Bot] - Disconnected! ")
 
     @bot.event
-    async def on_guild_join(guild):
-        prCyan(f'[Bot] - Guild join -> {guild.name}')
-        if os.path.exists(str(settings.MEDIA_ROOT)+'/images/'+str(guild.id)):
-            pass
-        else:
-            makedirs(str(settings.MEDIA_ROOT)+'/images/'+str(guild.id))
-        await DbConnection.fetch_server(guild)
-        await DbConnection.fetch_welcome(guild)
-
-    @bot.event 
-    async def on_guild_remove(guild):
-        prPurple(f'[Bot] - Guild Remove -> {guild.name}')
-        await DbConnection.left_server(guild)
-
-    @bot.event
     async def on_command(ctx:Context):
         cmd = str(ctx.command).replace(ctx.prefix,'').capitalize()
         if SAKURA_DEBUG:
