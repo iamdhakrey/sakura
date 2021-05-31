003b0c634f3ba6c0e071a2cc38cf5b222449835c
Hrithik Dhakrey <35266081+iamdhakrey@users.noreply.github.com>
2021-05-31 20:10:52 +0530

    update welcome

--- sakura/views.py
+++ sakura/views.py
@@ -1,10 +1,15 @@
+from sakura.decorator import role_required
 from django.http.response import JsonResponse
+from requests import api
 from sakura.models import Server, WelcomeData
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, permission_required
 from sakura.BotMics.api import Discord_API
 from django.http import request as Request
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from ast import literal_eval
+import requests
+from io import BytesIO
+from PIL import Image,ImageOps
 
 from django.conf import settings
 
@@ -12,11 +17,12 @@ discord_invite=settings.DISCORD_INVITE_LINK
 # Create your views here.
 @login_required
 def dashboard(request:Request):
+    print("redirect")
     content = {}
     for servers in Server.objects.all():
         if servers.admin is not None:
-            if request.user.id in  literal_eval(servers.admin):
-                print(request.user.id)
+            if request.user.id in literal_eval(servers.admin):
+                print(request.user.id,request.user)
     guild_list = []
     guild_list = Discord_API().get_guild_list(request.user.access_token)
     for key in guild_list:
@@ -25,7 +31,7 @@ def dashboard(request:Request):
             key['exists'] = True
         else:
             key['exists'] = False
-    print(guild_list)
+
     for servers in Server.objects.all():
         if servers.admin is not None:
             if request.user.id in  literal_eval(servers.admin):
@@ -39,15 +45,19 @@ def dashboard(request:Request):
     return render(request,'new_dashboard.html',{'username':request.user,'guild_list':guild_list,'invite_url':discord_invite})
 
 @login_required(redirect_field_name="login")
+@role_required(redirect_url='dashboard')
 def server(request,pk):
     if request.user.is_authenticated:
         guild = Server.objects.get(server_id=pk,)
-        print(guild.avatar)
         return render(request,'new_base.html',{'guild':guild})
 
 
+@login_required()
+@role_required(redirect_url='dashboard')
 def welcome(request:Request,pk):
     if request.user.is_authenticated:
+        success = None 
+        error = None
         if request.method =="POST":
             # print(request.POST)
             post_context = {}
@@ -64,25 +74,58 @@ def welcome(request:Request,pk):
                 welcome.welcome_msg = request.POST.get('welcome_message')
             if request.POST.get("welcome_role",None) is not None:
                 welcome.self_role =request.POST.get('welcome_role')
-            if request.POST.get("welcome_channel",None) is not None:
-                __image_link = request.POST.get('welcome_images')
+            if request.POST.get("welcome_images",None) is not None:
+                __image_link = request.POST.get('welcome_images').split(" ")
                 for image in __image_link:
                     if image == "":
                         __image_link.remove(image)
                 if len(__image_link) > 6:
                     __image_link = __image_link[0:4]
-                
+
+            image_name = []
+            success =True
+            i= 0
+            for link in __image_link:
+                try:
+                    if link == '':
+                        break
+                    i = i + 1
+                    url = requests.get(link)
+                    background = Image.open(BytesIO(url.content))
+                    width,height = background.size
+                    if width <= 1920 and height <= 972:
+                        error =  link+' is not set valid for background \nminimum 1920*972 resolution required'
+                        success = False
+                    else:
+                        background = background.resize((1920,972))
+                        output = ImageOps.fit(background,background.size,centering=(0.5,0.5))
+                        output.save("media/images/"+str(pk)+"/"+str(pk)+"_"+str(i)+".jpg")
+                        image_name.append(str(pk)+"_"+str(i)+".jpg")
+                        if i == 1:
+                            welcome.image1 ="images/"+str(pk)+"/"+str(pk)+"_"+str(i)+".jpg"
+                        elif i == 2:
+                            welcome.image2 ="images/"+str(pk)+"/"+str(pk)+"_"+str(i)+".jpg"
+                        elif i == 3:
+                            welcome.image3 ="images/"+str(pk)+"/"+str(pk)+"_"+str(i)+".jpg"
+                        elif i == 4:
+                            welcome.image4 ="images/"+str(pk)+"/"+str(pk)+"_"+str(i)+".jpg"
+                        elif i == 5:
+                            welcome.image5 ="images/"+str(pk)+"/"+str(pk)+"_"+str(i)+".jpg"
+                except requests.exceptions.MissingSchema:
+                    pass
+
+            if success:
+                welcome.update_by = request.user.id
                 welcome.save()
-
-            # WelcomeData.save()
-            
-
-            
+                success = "welcome message set successfully"
 
         text_channel = []
         context= {}
         data = Discord_API()
-
+        servers = Server.objects.get(pk=pk)
+        servers = Server.objects.filter(pk=pk).filter(admin__contains=str(request.user.id))
+        if servers is None:
+            return redirect('dashboard')
         sata = data.get_guild_channel(settings.TOKEN,id=pk)
         roles = data.get_guild_roles(settings.TOKEN,id=pk)
         welcome_data = WelcomeData.objects.get(server_id=pk)
@@ -92,24 +135,32 @@ def welcome(request:Request,pk):
             guild = guild
         )
         welcome_channel = {}
+
         for i in sata:
             # print((type(i['type'])))
             if i['type'] == 0:
 
                 text_channel.append(i)
-                if int(welcome_data.welcome_channel) == int(i['id']):
-                    welcome_channel['id'] = i['id'] 
-                    welcome_channel['name'] = i['name'] 
-        for role in roles:
-            if int(welcome_data.self_role) == int(role['id']):
-                welcome_channel['role_id'] = role['id']
-                welcome_channel['role_name'] = role['name']
+                try:
+                    if int(welcome_data.welcome_channel) == int(i['id']):
+                        welcome_channel['id'] = i['id'] 
+                        welcome_channel['name'] = i['name'] 
+                except TypeError:
+                    pass
+        try:
+            for role in roles:
+                if int(welcome_data.self_role) == int(role['id']):
+                    welcome_channel['role_id'] = role['id']
+                    welcome_channel['role_name'] = role['name']
+        except TypeError:
+            pass
         context = dict(
-            welcome = welcome_data,
-            guild = guild,
-            text_channel =text_channel,
-            roles = roles,
-            welcome_channel = welcome_channel
+            welcome         = welcome_data,
+            guild           = guild,
+            text_channel    = text_channel,
+            roles           = roles,
+            welcome_channel = welcome_channel,
+            error           = error,
+            success         = success
         )
-        # return JsonResponse(roles,safe=False)
         return render(request,'welcome.html',context) 
