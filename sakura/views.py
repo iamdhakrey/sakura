6f4ab9e9b4b7ff52be075a7230a477c7ebb5f26f
Hrithik Dhakrey <35266081+iamdhakrey@users.noreply.github.com>
2021-12-02 19:45:57 +0530

    [Backend] - change welcome view to class and add command view for bot help

--- sakura/views.py
+++ sakura/views.py
@@ -1,7 +1,7 @@
 from sakura.decorator import role_required
-from django.http.response import JsonResponse
+from django.http.response import HttpResponse, HttpResponseRedirect, JsonResponse
 from requests import api
-from sakura.models import Server, WelcomeData
+from sakura.models import HelpCmd, Server, WelcomeData
 from django.contrib.auth.decorators import login_required, permission_required
 from sakura.BotMics.api import Discord_API
 from django.http import request as Request
@@ -11,6 +11,9 @@ import requests
 from io import BytesIO
 from PIL import Image, ImageOps
 
+
+from django.views.generic import TemplateView,View
+
 from django.conf import settings
 
 discord_invite = settings.DISCORD_INVITE_LINK
@@ -19,14 +22,16 @@ discord_invite = settings.DISCORD_INVITE_LINK
 
 @login_required
 def dashboard(request: Request):
-    print("redirect")
+    print("redirect to dashboard")
     content = {}
-    for servers in Server.objects.all():
-        if servers.admin is not None:
-            if request.user.id in literal_eval(servers.admin):
-                print(request.user.id, request.user)
+    # for servers in Server.objects.all():
+    #     if servers.admin is not None:
+    #         if request.user.id in literal_eval(servers.admin):
+    #             print(request.user.id, request.user)
     guild_list = []
     guild_list = Discord_API().get_guild_list(request.user.access_token)
+    print(guild_list)
+    print(Discord_API().check_token(request.user.access_token))
     for key in guild_list:
         check = Server.objects.get(server_id=int(key['id']))
         if check.is_active:
@@ -43,7 +48,13 @@ def dashboard(request: Request):
                     icon=servers.avatar,
                     exists=servers.is_active
                 )
-                guild_list.append(temp_server)
+        for guild in guild_list:
+            if str(servers.server_id) == guild['id'] and servers.avatar !=guild['icon']:
+                servers.server_name = guild['name']
+                servers.avatar = guild['icon']
+                # save data in datavase
+                servers.save()
+                # guild_list.append(temp_server)
     return render(request, 'new_dashboard.html', {'username': request.user, 'guild_list': guild_list, 'invite_url': discord_invite})
 
 
@@ -175,5 +186,93 @@ def welcome(request: Request, pk):
             welcome_channel=welcome_channel,
             error=error,
             success=success
+        )
+        return render(request, 'welcome.html', context)
+    
+class CommandView(View):
+    template_name = 'command.html'
+    
+    def get(self, request):
+        # if request.user.is_authenticated:
+        help_cmds = HelpCmd.objects.all()
+        # get unique categories in help_cmds
+        categories = set(
+            [help_cmd.category for help_cmd in help_cmds])
+        # guild = Server.objects.get(server_id=pk,
+        
+        return render(request, 'command.html',{'helpcmds':help_cmds,'categories':categories})
+        # else:
+            # return redirect('auth')
+            
+            
+@login_required()
+@role_required(redirect_url='dashboard')
+def enable_welcome_msg(request, pk):
+    if request.method == "POST":
+        # get data form request
+        post_context = {}
+        welcome = WelcomeData.objects.get(server_id=int(pk))
+        if request.POST.get("enable") is not None:
+            if request.POST.get('enable') == "1":
+                welcome.welcome_enable = True
+            else:
+                welcome.welcome_enable = False
+        welcome.update_by = request.user.id
+        welcome.save()
+        return JsonResponse({'success': True})
+    else:
+        # redirect to previous page
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER','/'))
+
+
+class WelcomeView(View):
+    template_name = 'welcome.html'
+    
+    def get(self,request:Request,pk):
+        # get welcome data 
+        
+        welcome_data = WelcomeData.objects.get(server_id=int(pk))
+        print(welcome_data)
+        # get guild
+        # guild = Server.objects.get(server_id=pk,admin__contains=str(request.user.id))
+        # get channels
+        text_channel = []
+        context = {}
+        data = Discord_API()
+        servers = Server.objects.get(pk=pk)
+        servers = Server.objects.filter(pk=pk).filter(
+            admin__contains=str(request.user.id))
+        if servers is None:
+            return redirect('dashboard')
+        sata = data.get_guild_channel(settings.TOKEN, id=pk)
+        roles = data.get_guild_roles(settings.TOKEN, id=pk)
+        welcome_channel = {}
+        
+        for i in sata:
+            # print((type(i['type'])))
+            if i['type'] == 0:
+
+                text_channel.append(i)
+                try:
+                    if int(welcome_data.welcome_channel) == int(i['id']):
+                        welcome_channel['id'] = i['id']
+                        welcome_channel['name'] = i['name']
+                except TypeError:
+                    pass
+        try:
+            for role in roles:
+                if int(welcome_data.self_role) == int(role['id']):
+                    welcome_channel['role_id'] = role['id']
+                    welcome_channel['role_name'] = role['name']
+        except TypeError:
+            pass
+        context = dict(
+            welcome=welcome_data,
+            # guild=guild,
+            text_channel=text_channel,
+            roles=roles,
+            welcome_channel=welcome_channel,
+            error=None,
+            success=None
         )
         return render(request, 'welcome.html', context)
